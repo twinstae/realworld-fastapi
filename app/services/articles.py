31f5f836bc6239e075e1eb901c173881db3fb443
twinstae <rabolution@gmail.com>
2021-04-07 15:39:51 +0900

    get_article_by_slug_from_path 추출

--- app/services/articles.py
+++ app/services/articles.py
@@ -1,5 +1,19 @@
+from fastapi import Path, HTTPException
 from slugify import slugify
+from starlette.status import HTTP_404_NOT_FOUND
+from app.models.orm import Article
+from app.resources import strings
 
 
 def get_slug_for_article(title: str) -> str:
     return slugify(title)
+
+
+async def get_article_by_slug_from_path(slug: str = Path(..., min_length=1)) -> Article:
+    article = await Article.get_or_none(slug=slug).prefetch_related("author")
+    if article is None:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND,
+            detail=strings.WRONG_SLUG_NO_ARTICLE
+        )
+    return article
